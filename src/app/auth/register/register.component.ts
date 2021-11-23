53edd5c2f2be7cef235cc4ab41f04b0aa98e263d
Paulina Guerrero <paaulinagp@gmail.com>
2021-11-22 21:48:58 -0600

    feat: registrando usuarios

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,11 @@ export class RegisterComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(private _fb: FormBuilder) {
+  constructor(
+    private _fb: FormBuilder, 
+    private _router: Router, 
+    private _authService: AuthService
+  ) {
     this.formGroup = this._fb.group({
       nombre: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -22,7 +28,18 @@ export class RegisterComponent implements OnInit {
   }
 
   createUser(){
-    console.log(this.formGroup.value);
+    if(this.formGroup.invalid){
+      return;
+    }
+
+    const { nombre, email, password } = this.formGroup.value;
+
+    this._authService.createUser(nombre, email, password)
+    .then((credentials) => {
+      console.log(credentials);
+      this._router.navigate(['/dashboard']);
+    })
+    .catch((error) => console.log(error));
     
   }
 
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _auth: AngularFireAuth) { }
+
+
+  createUser(nombre: string, email: string, password: string) {
+    return this._auth.createUserWithEmailAndPassword(email, password);
+  }
+}
